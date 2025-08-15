31846e000d9f6fca4aa0c10e06e5fb3722218e79
inglide-noberto <inglidenoberto961@gmail.com>
2025-08-14 22:03:11 -0300

    feat: adicionar módulos de autenticação e usuários, e implementar funcionalidades de tarefas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { TasksModule } from './modules/tasks/tasks.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
