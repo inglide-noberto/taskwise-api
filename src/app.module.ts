4dc9b1992cd13fc5529174fd8c6672173bc262b5
inglide-noberto <inglidenoberto961@gmail.com>
2025-08-14 23:18:24 -0300

    feat: adicionar módulo de autenticação com controller, service e DTOs; configurar conexão com MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { TasksModule } from './modules/tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule, AuthModule, UsersModule, TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
